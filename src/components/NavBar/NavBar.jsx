5a22bf243613b170dd349a380625e2b35e7ea739
arynyestos <arynyestos@gmail.com>
2023-10-08 03:39:47 +0200

    NavBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import './Navbar.css';
+import './NavBar.css';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 function Navbar() {
